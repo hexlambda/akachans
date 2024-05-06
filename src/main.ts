bbcde8688b006d6508f2bf98199cbc48939fc952
hexlambda <hexlambda@outlook.com>
2024-05-06 19:20:11 +0530

    Added icons

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import "@fontsource/days-one";
-import "@fortawesome/fontawesome-free/css/all.css";
+import "./icons.css";
 
 const generateBackgroundTiles = () => {
   const baseFontSize = Number.parseFloat(
